422bc3f43405c54e6f65600f3a54ea9e780c188d
juan-ma526 <jmperez675@gmail.com>
2022-11-18 23:36:48 -0300

    se mejoro la vista, y se adecuo a 1920

--- pages/user/cancelled.jsx
+++ pages/user/cancelled.jsx
@@ -3,101 +3,190 @@ import React from "react";
 
 const cancelled = () => {
   return (
-    <div className="containerCancelled">
-      <div className="divFlecha">
-        <div>
-          <Link href="#">
-            <svg
-              width="17"
-              height="16"
-              viewBox="0 0 17 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8.70509 15.705C9.09445 15.3156 9.09445 14.6844 8.70509 14.295L3.41009 9H16.0001C16.5524 9 17.0001 8.55228 17.0001 8C17.0001 7.44771 16.5524 7 16.0001 7H3.41009L8.70509 1.705C9.09445 1.31564 9.09445 0.684357 8.70509 0.294996C8.31573 -0.0943644 7.68445 -0.0943643 7.29509 0.294997L0.297195 7.29289C-0.0933288 7.68341 -0.0933296 8.31658 0.297195 8.7071L7.29509 15.705C7.68445 16.0944 8.31573 16.0944 8.70509 15.705Z"
-                fill="#A442F1"
-              />
-            </svg>
-          </Link>
-        </div>
-        <p>Atras</p>
+    <div className="container-cancelado">
+      <div className="cancelado-vector">
+        <svg
+          className="cancelado-user-interface"
+          width="15"
+          height="14"
+          viewBox="0 0 15 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.58704 13.4209C7.91151 13.0964 7.91151 12.5703 7.58704 12.2459L3.17454 7.83337H13.6662C14.1264 7.83337 14.4995 7.46027 14.4995 7.00003C14.4995 6.53979 14.1264 6.1667 13.6662 6.1667H3.17454L7.58704 1.7542C7.91151 1.42973 7.91151 0.903667 7.58704 0.579199C7.26258 0.254732 6.73651 0.254732 6.41204 0.579199L0.698317 6.29293C0.307793 6.68345 0.307793 7.31661 0.698318 7.70714L6.41204 13.4209C6.73651 13.7453 7.26258 13.7453 7.58704 13.4209Z"
+            fill="#A442F1"
+          />
+        </svg>
+
+        <span className="cancelado-text-vector">
+          <span>Atrás</span>
+        </span>
       </div>
-      {/* Informacion reserva */}
-      <div className="infoReserva">
-        <div>
-          <span className="spanInfo1">Informacion de la reserva</span>
-          <span className="spanInfo2">Ivan Cruce</span>
+      <div className="div-cancelar">
+        <div className="cancelado-header">
+          <span className="cancelado-nombre">
+            <span>Hola Ivan,</span>
+          </span>
+          <span className="cancelado-pregunta">
+            <span>¿Por qué desea cancelar su reserva?</span>
+          </span>
         </div>
-        <div className="contentReserva">
-          <div className="contentReserva1">
-            <div>
-              <span className="spanReserva1">Dia</span>
-              <span className="spanReserva2">12/10/2022</span>
-            </div>
-          </div>
-          <div className="contentReserva2">
-            <span className="spanReserva1">Horario</span>
-            <span className="spanReserva2">13:00hs</span>
-          </div>
-          <div className="contentReserva3">
-            <span className="spanReserva1">Sucursal</span>
-            <span className="spanReserva2">Villa Crespo</span>
-          </div>
+        <svg
+          className="cancelado-border "
+          width="779"
+          height="1"
+          viewBox="0 0 779 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line y1="0.5" x2="779" y2="0.5" stroke="#C8C8C8" />
+        </svg>
+
+        <div className="cancelado-div-checkbox">
+          <input className="cancelado-user-interface1" type="checkbox" />
+
+          <span className="cancelado-text06">
+            <span>Ya no quiero ir</span>
+          </span>
         </div>
-      </div>
-      <div className="cancelarReserva">Cancelar Reserva</div>
-      {/* div cancelar */}
-      <div className="cancelar">
-        {/* contenido div */}
-        <div className="cancelarHeader">
-          <div className="tituloHeader">Hola Ivan</div>
-          <div className="preguntaHeader">
-            ¿Porque desea cancelar su reserva?
-          </div>
-          <div className="border"></div>
+        <svg
+          className="cancelado-border1"
+          width="779"
+          height="1"
+          viewBox="0 0 779 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line y1="0.5" x2="779" y2="0.5" stroke="#C8C8C8" />
+        </svg>
+
+        <div className="cancelado-div-checkbox2">
+          <input className="cancelado-user-interface1" type="checkbox" />
+          <span className="cancelado-text08">
+            <span>Me equivoqué de horario</span>
+          </span>
         </div>
-        <div className="checkboxDiv">
-          <div>
-            <input type="checkbox" />
-          </div>
-          <span>Ya no quiero ir</span>
-        </div>
-        <div className="border1"></div>
-        <div className="checkboxDiv">
-          <div>
-            <input type="checkbox" />
-          </div>
-          <span>Me Equivoque de horario</span>
-        </div>
-        <div className="card">
-          <div className="divCard1">Su reserva actual será cancelada</div>
-          <div className="divCard2">La cancelacion no puede ser revertida</div>
-          <button>
-            <span>Confirmar Cancelacion</span>
+        <div className="cancelado-card">
+          <span className="cancelado-text10">
+            <span>Su reserva actual será cancelada</span>
+          </span>
+          <span className="cancelado-text12">
+            <span>La cancelación no puede ser revertida</span>
+          </span>
+          <button className="cancelado-button-red">
+            <span className="cancelado-span-text">
+              <span>Confirmar cancelación</span>
+            </span>
           </button>
         </div>
-        <div className="border2"></div>
-        <div className="checkboxDiv2">
-          <div>
-            <input type="checkbox" />
-          </div>
-          <span>Encontre un lugar mejor</span>
+        <svg
+          className="cancelado-border2"
+          width="779"
+          height="1"
+          viewBox="0 0 779 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line y1="0.5" x2="779" y2="0.5" stroke="#C8C8C8" />
+        </svg>
+
+        <div className="cancelado-div-checkbox3">
+          <input className="cancelado-user-interface1" type="checkbox" />
+          <span className="cancelado-text16">
+            <span>Encontré un lugar mejor</span>
+          </span>
         </div>
-        <div className="border3"></div>
-        <div className="checkboxDiv3">
-          <div>
-            <input type="checkbox" />
-          </div>
-          <span>Me cancelaron</span>
+        <svg
+          className="cancelado-border3"
+          width="779"
+          height="1"
+          viewBox="0 0 779 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line y1="0.5" x2="779" y2="0.5" stroke="#C8C8C8" />
+        </svg>
+
+        <div className="cancelado-div-checkbox4">
+          <input className="cancelado-user-interface1" type="checkbox" />
+          <span className="cancelado-text18">
+            <span>Me cancelaron</span>
+          </span>
         </div>
-        <div className="border4"></div>
-        <div className="checkboxDiv4">
-          <div>
-            <input type="checkbox" />
-          </div>
-          <span>Otro</span>
+        <svg
+          className="cancelado-border4"
+          width="779"
+          height="1"
+          viewBox="0 0 779 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line y1="0.5" x2="779" y2="0.5" stroke="#C8C8C8" />
+        </svg>
+
+        <div className="cancelado-div-checkbox5">
+          <input className="cancelado-user-interface1" type="checkbox" />
+          <span className="cancelado-text20">
+            <span>Otro</span>
+          </span>
         </div>
+      </div>
+      <span className="cancelado-text22">
+        <span>Cancelar reserva</span>
+      </span>
+      <div className="cancelado-div-informacinreserva">
+        <div className="cancelado-datos-usuario">
+          <span className="cancelado-text24">
+            <span>Información de la reserva</span>
+          </span>
+          <span className="cancelado-text26">
+            <span>Ivan Cruce</span>
+          </span>
+        </div>
+        <div className="cancelado-div-content">
+          <div className="cancelado-div-txt1">
+            <span className="cancelado-text28">
+              <span>Día</span>
+            </span>
+            <span className="cancelado-text30">
+              <span>12/10/2022</span>
+            </span>
+          </div>
+          <div className="cancelado-div-txt2">
+            <span className="cancelado-text32">
+              <span>Horario:</span>
+            </span>
+            <span className="cancelado-text34">
+              <span>13:00 hs</span>
+            </span>
+          </div>
+          <div className="cancelado-div-txt3">
+            <span className="cancelado-text36">
+              <span>Sucursal:</span>
+            </span>
+            <span className="cancelado-text38">
+              <span>Villa Crespo</span>
+            </span>
+          </div>
+        </div>
+        <svg
+          className="cancelado-border5"
+          width="282"
+          height="1"
+          viewBox="0 0 282 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line
+            x1="0.5"
+            y1="0.5"
+            x2="281.5"
+            y2="0.5"
+            stroke="#E1E1E1"
+            stroke-linecap="round"
+          />
+        </svg>
       </div>
     </div>
   );
