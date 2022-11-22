402b80c64eed910a0485b16fce8ba5db1414b495
juan-ma526 <jmperez675@gmail.com>
2022-11-22 19:10:39 -0300

    creacion de token

---
+++ api/config/token.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+//Clave secreta
+
+const SECRET = "Xruce";
+
+//Generamos el token, recibiendo el payload q guardamos ahi.
+
+const generateToken = (payload) => {
+  const token = jwt.sign(payload, SECRET, { expiresIn: "1d" });
+
+  return token;
+};
+
+//Validamos el token
+const validateToken = (token) => {
+  return jwt.verify(token, SECRET);
+};
+
+module.exports = { generateToken, validateToken };
