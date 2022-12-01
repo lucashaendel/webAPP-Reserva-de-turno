7a8c29a220f4e9da36bb23c620df868dbfb3dd97
lucas h <lucas.ph@live.com>
2022-12-01 11:23:37 -0300

    se modifico el cors

--- api/server.js
+++ api/server.js
@@ -7,7 +7,13 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use("/api", routers);
 mongoose
